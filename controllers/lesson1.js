33a7d1bd95056b49b9d59437a98c377d552968b7
luisalfredoestigarribiasosa85 <luisalfredoestigarribiasosa85@gmail.com>
2025-01-08 20:40:31 -0300

    added a route to the name of my daughter

--- controllers/lesson1.js
+++ controllers/lesson1.js
@@ -6,7 +6,12 @@ const luisRoute = (req, res) => {
     res.send("Hello, Luis");
 };
 
+const ashleyRoute = (req, res) => {
+    res.send("Hello, Ashley");
+};
+
 module.exports = {
     kariRoute,
-    luisRoute
+    luisRoute,
+    ashleyRoute
 };
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const lesson1Controller = require('../controllers/lesson1');
 
 routes.get('/', lesson1Controller.luisRoute);
 routes.get('/kari', lesson1Controller.kariRoute);
+routes.get('/ashley', lesson1Controller.ashleyRoute);
 
 module.exports = routes;
