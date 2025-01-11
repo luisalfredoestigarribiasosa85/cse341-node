59f23cde686b3da027212bb94c4bda650f4eda7f
luisalfredoestigarribiasosa85 <luisalfredoestigarribiasosa85@gmail.com>
2025-01-11 16:40:28 -0300

    Developing an APi

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use('/', require('./routes/index'));
-
-const port = 10000;
-app.listen(process.env.PORT || port, () => {
-    console.log('Web Server is listening at port ' + (process.env.PORT || port));
-});
--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const routes = require('express').Router();
-const lesson1Controller = require('../controllers/lesson1');
-
-routes.get('/', lesson1Controller.luisRoute);
-routes.get('/kari', lesson1Controller.kariRoute);
-routes.get('/ashley', lesson1Controller.ashleyRoute);
-
-module.exports = routes;
